5bc4cf4d132be5fb0ed353aabc2753580d921a10
kubajak <drobnik87@gmail.com>
2024-03-07 12:18:37 +0100

    More Code

--- src/app/list.php
+++ src/app/list.php
@@ -18,6 +18,7 @@
     <script defer src="../js/confirmDelete.js"></script>
     <script defer src="../js/confirmEdit.js"></script>
     <script defer src="../js/ShowCurrentYear.js"></script>
+    <script defer src="../js/liveSearch.js"></script>
 </head>
 
 <body>
@@ -58,47 +59,6 @@
             </div>
         </div>
         <div id="searchResults"></div>
-        <script>
-            var timeout;
-
-            async function liveSearch() {
-                var input = document.getElementById("searchInput").value;
-                var resultsDiv = document.getElementById("searchResults");
-
-                // Jeśli pole wyszukiwania jest puste, pobierz wszystkie wartości
-                if (input.length === 0) {
-                    clearTimeout(timeout);
-
-                    timeout = setTimeout(async function() {
-                        try {
-                            var response = await fetch("classes/Livesearch.class.php");
-                            var data = await response.text();
-                            resultsDiv.innerHTML = data;
-                        } catch (error) {
-                            console.error("Error fetching data:", error);
-                        }
-                    }, 300);
-                } else {
-                    // Jeśli pole wyszukiwania nie jest puste, wykonaj standardowe wyszukiwanie
-                    if ((input.length >= 1)) {
-                        clearTimeout(timeout);
-
-                        timeout = setTimeout(async function() {
-                            try {
-                                var sanitizedInput = encodeURIComponent(input);
-                                var response = await fetch("classes/Livesearch.class.php?q=" + sanitizedInput);
-                                var data = await response.text();
-                                resultsDiv.innerHTML = data;
-                            } catch (error) {
-                                console.error("Error fetching data:", error);
-                            }
-                        }, 300);
-                    }
-                }
-            }
-
-            liveSearch();
-        </script>
     </div>
 </body>
 
